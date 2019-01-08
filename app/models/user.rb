5de3e2352fb641df0eee6b0b63d00042c68312b2
thanh218 <trinhhoangthanh218@gmail.com>
2019-01-08 17:00:50 +0700

    Chapter7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name,  presence: true, length: {maximum: Setting.name_maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Setting.email_maximum}
-  format: { with: VALID_EMAIL_REGEX }
-  uniqueness: { case_sensitive: false }
+  validates :name,  presence: true, length: {maximum: Settings.name_maximum}
+  validates :password, presence: true, length: {maximum: Settings.pass_maximum}
+  validates :email, presence: true, length: {maximum: Settings.email_maximum},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  before_save :downcase_email
   has_secure_password
-  validates :password, presence: true, length: {maximum: Setting.pass_maximum}
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
