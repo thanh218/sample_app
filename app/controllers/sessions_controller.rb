834bd5847d514f5a05bd4734cc36c823c3d59295
thanh218 <trinhhoangthanh218@gmail.com>
2019-01-08 17:00:50 +0700

    Chapter11: Account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
 
     if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == Settings.one ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        user_activated user
+      else
+        flash[:warning] = t ".content2"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "content1"
       render :new
@@ -17,5 +20,13 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private 
+
+  def user_activated user
+    log_in user
+    params[:session][:remember_me] == Settings.one ? remember(user) : forget(user)
+    redirect_back_or user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      log_in @user
-      flash[:success] = t ".content1"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t ".statusmail"
+      redirect_to root_url
     else
       flash[:notice] = t ".content2"
       render :new
